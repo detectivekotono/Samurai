5b1130a476eb539dfef7dab2aa32968cb8269b12
detectivekotono <151484660+detectivekotono@users.noreply.github.com>
2024-06-24 12:14:50 +0000

    修正

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,10 +1,11 @@
-let num=15
-if(num%3)===0&&(num%5)==0{
+let num=10
+
+if((num%3)==0&&(num%5)==0){
     console.log("3と5の倍数です");
-    elseif(num%5===0){
+ } else if((num%5)==0){
         console.log("5の倍数です");
-        elseif(num%3===0){
+ }else if((num%3)==0){
 console.log("3の倍数です");
+        }else{
+            console.log(num);
         }
-    }
-}
