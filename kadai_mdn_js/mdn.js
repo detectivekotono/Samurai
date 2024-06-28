e97814632c3ced518793427fb7e2613442795e31
detectivekotono <151484660+detectivekotono@users.noreply.github.com>
2024-06-28 22:13:19 +0900

    修正

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,6 +1,8 @@
-const date = new Date("2000-01-17T16:45:30");
-const [month, day, year] = [
-  date.getMonth(),
-  date.getDate(),
-  date.getFullYear(),
-];
+function displayCurrentDate() {
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = now.getMonth() + 1;
+  const day = now.getDate();
+  console.log();
+}
+displayCurrentDate();
