494bffb8cbffa6186bb1322f0ce600fa5728cb35
detectivekotono <151484660+detectivekotono@users.noreply.github.com>
2024-06-28 22:18:20 +0900

    修正

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -3,6 +3,6 @@ function displayCurrentDate() {
   const year = now.getFullYear();
   const month = now.getMonth() + 1;
   const day = now.getDate();
-  console.log();
+  console.log(`現在の年月日は${year}年${month}月${day}日です`);
 }
 displayCurrentDate();
