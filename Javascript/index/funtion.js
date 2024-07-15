5d5f2571081fe0c1aa052c85478483da8b0ceaea
detectivekotono <151484660+detectivekotono@users.noreply.github.com>
2024-07-15 15:27:14 +0900

    修正

---
+++ Javascript/index/funtion.js
@@ -0,0 +1,17 @@
+//朝の挨拶を出力する関数を定義する
+const sayGoodMorning = () => {
+  console.log(`おはようございます！`);
+  console.log(`昨日はよく眠れましたか？`);
+  console.log(`今日も一日頑張りましょう！`);
+};
+
+//夜のあいさつを出力する関数を定義する
+const sayGoodEvening = () => {
+  console.log(`こんばんわ！`);
+  console.log(`今日も一日おつかれさまでした。`);
+};
+//朝の挨拶を出力する関数を呼び出す
+sayGoodMorning();
+
+//夜の挨拶を出力する関数を呼び出す
+sayGoodEvening();
