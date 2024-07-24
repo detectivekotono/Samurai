79320e67131fa0260a504d486c5bb6ebc2717cee
detectivekotono <151484660+detectivekotono@users.noreply.github.com>
2024-07-24 22:34:48 +0900

    追記

--- Python/sample.py
+++ Python/sample.py
@@ -6,3 +6,22 @@ user_names.append("侍五郎")#6番目の要素を追加
 print(user_names)
 user_names.pop(2)#3番目の要素を削除(0始まりのため2を指定)
 print(user_names)
+
+for i in range(1,11):
+    print(i)
+    if i==5:
+        break
+    
+    import random
+
+for i in range(1,11):
+    num = random.randint(1,20)
+    print(f"{i}回目の結果は{num}です。")        
+    if num == 20:
+        print("20が出たので繰り返し処理を強制終了します。")
+        break
+    
+for i in range(1,11):
+    if i %2==1:
+        continue
+    print(i)
