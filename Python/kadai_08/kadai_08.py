3cab69e71c10314beb44999daee589f9fd96526e
detectivekotono <151484660+detectivekotono@users.noreply.github.com>
2024-07-24 21:49:23 +0900

    修正

--- Python/kadai_08/kadai_08.py
+++ Python/kadai_08/kadai_08.py
@@ -1,4 +1,16 @@
-if 条件式:
-    条件が成り立つときの処理
+var=1
+
+if  var % 3==0 and var %5==0:
+    print("BuzzFizz")
+elif var  % 3==0:
+    print("Fizz")
+elif var % 5==0:
+    print("Buzz")
 else:
-    条件が成り立たないときの処理
+    print(var)
+#if 条件式A:
+ #   条件Aが成り立つときの処理
+#elif 条件式B:
+ #   条件Bが成り立つときの処理
+#else:
+#    どの条件も成り立たないときの処理    
